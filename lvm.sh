58af1b264640b163187fc66cedd42d0ad500e34d
sagarcjain <40575312+sagarcjain@users.noreply.github.com>
2018-06-26 10:20:52 +0200

    Update lvm.sh

--- lvm.sh
+++ lvm.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 #create lvm snapshot
 
@@ -6,8 +7,8 @@ function LvmSnapshotBackup
 
 VG=$(vgs -o vg_name --noheadings | awk 'FNR == 1 {print}' | sed 's/ //g') # LVM volume group we are snapshoting
 LV=$(lvs -o lv_name --noheadings | awk 'FNR == 1 {print}' | sed 's/ //g') # Name of LVM-logical volume to take a snapshot of
-VG_TOTAL=$(vgs --noheadings | awk {'print $6'} | cut -d'.' -f1) #To check total VG size
-VG_USED=$(vgs --noheadings | awk {'print $7'} | cut -d'.' -f1) #To check usage of VG 
+VG_TOTAL=$(vgs --units m | awk 'NR > 1 {print int($6)}') #To check total VG size
+VG_USED=$(vgs --units m | awk 'NR > 1 {print int($7)}') #To check usage of VG 
 percent=$(awk "BEGIN { pc=100*${VG_USED}/${VG_TOTAL}; i=int(pc); print (pc-i<0.5)?i:i+1 }") #To print how much % VG is used
 BACKUP_PREFIX="lvm_snapshot_backup-" # Prefix of snapshot volume name.
 SIZE=50G # Amount of disk space to allocate for the snapshot
@@ -24,16 +25,14 @@ else
 fi
 }
 
-
-
-
-
 # Cleanup old snapshots
 
 function LvmSnapshotBackupDelete
 
 {
-VG=$(vgs -o vg_name --noheadings | awk 'FNR == 1 {print}' | sed 's/ //g')
-lvmsnapfull=$(lvs --noheadings -o lv_name,snap_percent | grep lvm_snap | grep 100 |  awk {'print $1'})
-		/sbin/lvremove -f "$VG/$lvmsnapfull"
+	lvmsnapfull=$(lvs --noheadings -o lv_name,snap_percent  2>/dev/null | grep lvm_snap | grep 100 |  awk {'print $1'})
+	VG=$(vgs -o vg_name --noheadings 2>/dev/null | awk 'FNR == 1 {print}' | sed 's/ //g')
+
+		echo "$lvmsnapfull" | while read i; do echo "Deleting 100% full snapshots $i" && /sbin/lvremove -f "$VG/$i"; done
+
 }
